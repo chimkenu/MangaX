7cf06754646a8d411c786a84cfdb30fb838cf084
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-06-05 00:51:26 +0800

    Added move event listeners

--- src/main/java/me/chimkenu/mangax/listeners/MoveListener.java
+++ src/main/java/me/chimkenu/mangax/listeners/MoveListener.java
@@ -5,12 +5,18 @@ import me.chimkenu.mangax.characters.Move;
 import me.chimkenu.mangax.enums.Moves;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class MoveListener extends GameListener {
     public MoveListener(JavaPlugin plugin) {
         super(plugin);
+
+        for (Moves move : Moves.values()) {
+            if (move.move instanceof Listener listener)
+                plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+        }
     }
 
     private void activateMove(Player player) {
