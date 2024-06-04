6718a31680f7188ea4756283a8b06929e1411535
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-06-05 00:43:43 +0800

    Added equipment locks

--- src/main/java/me/chimkenu/mangax/ArmorStandUtil.java
+++ src/main/java/me/chimkenu/mangax/ArmorStandUtil.java
@@ -2,6 +2,7 @@ package me.chimkenu.mangax;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class ArmorStandUtil {
     public static void setUpArmorStand(ArmorStand armorStand) {
@@ -10,6 +11,12 @@ public class ArmorStandUtil {
         armorStand.setGravity(false);
         armorStand.setSilent(true);
         armorStand.setBasePlate(false);
+        armorStand.addEquipmentLock(EquipmentSlot.HEAD, ArmorStand.LockType.REMOVING_OR_CHANGING);
+        armorStand.addEquipmentLock(EquipmentSlot.CHEST, ArmorStand.LockType.REMOVING_OR_CHANGING);
+        armorStand.addEquipmentLock(EquipmentSlot.LEGS, ArmorStand.LockType.REMOVING_OR_CHANGING);
+        armorStand.addEquipmentLock(EquipmentSlot.FEET, ArmorStand.LockType.REMOVING_OR_CHANGING);
+        armorStand.addEquipmentLock(EquipmentSlot.HAND, ArmorStand.LockType.REMOVING_OR_CHANGING);
+        armorStand.addEquipmentLock(EquipmentSlot.OFF_HAND, ArmorStand.LockType.REMOVING_OR_CHANGING);
     }
 
     public static void runCommand(String command) {
