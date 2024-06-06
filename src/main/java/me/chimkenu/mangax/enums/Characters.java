c1fe4658c60df255fa756f1b0cab2c73bf368e8e
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-06-06 10:34:39 +0800

    Removed use of deprecated api method

--- src/main/java/me/chimkenu/mangax/enums/Characters.java
+++ src/main/java/me/chimkenu/mangax/enums/Characters.java
@@ -3,6 +3,8 @@ package me.chimkenu.mangax.enums;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Objects;
+
 public enum Characters {
     JOTARO,
     TANJIRO,
@@ -18,7 +20,7 @@ public enum Characters {
         if (meta == null || !meta.hasDisplayName())
             return null;
 
-        String s = meta.getDisplayName();
+        String s = Objects.requireNonNull(meta.displayName()).examinableName();
         for (Characters character : Characters.values()) {
             if (character.toString().toLowerCase().contains(s.toLowerCase()))
                 return character;
