bd1773eddae5a5522218ccbac12744cd79349313
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-06-13 12:23:56 +0800

    Added event call

--- src/main/java/me/chimkenu/mangax/characters/todoroki/FlashfireFist.java
+++ src/main/java/me/chimkenu/mangax/characters/todoroki/FlashfireFist.java
@@ -2,6 +2,8 @@ package me.chimkenu.mangax.characters.todoroki;
 
 import me.chimkenu.mangax.characters.Move;
 import me.chimkenu.mangax.enums.MoveInfo;
+import me.chimkenu.mangax.enums.Moves;
+import me.chimkenu.mangax.events.MoveTargetEvent;
 import me.chimkenu.mangax.utils.ParticleEffects;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
@@ -20,20 +22,34 @@ public class FlashfireFist extends Move {
             loc.add(loc.getDirection().multiply(2));
             for (LivingEntity e : loc.getNearbyLivingEntities(1)) {
                 if (e != entity && !e.getType().equals(EntityType.ARMOR_STAND)) {
+                    MoveTargetEvent event;
+
                     // Do more damage if entity is already on fire
                     double multiplier = 1;
                     if (e.getFireTicks() > 0) {
                         multiplier = 1.5;
+                        event = new MoveTargetEvent(Moves.TODOROKI_FLASHFIRE_FIST, entity, e, 6 * multiplier, entity.getLocation().getDirection().multiply(multiplier));
+                        Bukkit.getPluginManager().callEvent(event);
+                        if (event.isCancelled()) {
+                            return;
+                        }
+
                         e.getWorld().playSound(e, Sound.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS, 1, 0.5f);
                         e.getWorld().spawnParticle(Particle.LARGE_SMOKE, e.getEyeLocation(), 100, 0.3, 0.2, 0.3, 0.1);
                     } else {
+                        event = new MoveTargetEvent(Moves.TODOROKI_FLASHFIRE_FIST, entity, e, 6 * multiplier, entity.getLocation().getDirection().multiply(multiplier));
+                        Bukkit.getPluginManager().callEvent(event);
+                        if (event.isCancelled()) {
+                            return;
+                        }
+
                         e.getWorld().playSound(e, Sound.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS, 0.5f, 1);
                         e.getWorld().spawnParticle(Particle.SMOKE, e.getEyeLocation(), 100, 0.3, 0.2, 0.3, 0.1);
                     }
 
-                    e.damage(6 * multiplier, entity);
-                    e.setFireTicks((int) (40 * multiplier));
-                    e.setVelocity(e.getVelocity().add(entity.getLocation().getDirection().multiply(multiplier)));
+                    event.getTarget().damage(event.getDamage(), event.getSource());
+                    event.getTarget().setFireTicks((int) (40 * multiplier));
+                    e.setVelocity(e.getVelocity().add(event.getKnockback()));
                 }
             }
         }, null, 0, 5 * 20, Material.MAGMA_BLOCK, Component.text("Flashfire Fist").color(TextColor.color(0xff6200)).decorate(TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false));
