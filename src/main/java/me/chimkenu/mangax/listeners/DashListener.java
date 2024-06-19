47441afc1350c394d34f00b95f453bb5b8b49e4b
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-06-20 00:27:38 +0800

    Added regen cost

--- src/main/java/me/chimkenu/mangax/listeners/DashListener.java
+++ src/main/java/me/chimkenu/mangax/listeners/DashListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -76,6 +77,13 @@ public class DashListener implements Listener {
     public void onFoodLevelChange(FoodLevelChangeEvent e) {
         if (e.getEntity() instanceof Player player) {
             regenerate(player);
+        }
+    }
+
+    @EventHandler
+    public void onRegenerate(EntityRegainHealthEvent e) {
+        if (e.getEntity() instanceof Player player && e.getRegainReason() == EntityRegainHealthEvent.RegainReason.SATIATED) {
+            player.setFoodLevel(player.getFoodLevel() - 4);
         }
     }
 
