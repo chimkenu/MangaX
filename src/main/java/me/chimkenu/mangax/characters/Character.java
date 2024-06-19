65e220881e63a0b4c57c6910b8dfe584d56c564c
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-06-19 14:07:03 +0800

    Removed character class

--- src/main/java/me/chimkenu/mangax/characters/Character.java
+++
@@ -1,7 +0,0 @@
-package me.chimkenu.mangax.characters;
-
-import java.util.Set;
-
-public abstract class Character {
-    Set<Move> moveSet;
-}
--- src/main/java/me/chimkenu/mangax/characters/deku/Deku.java
+++
@@ -1,6 +0,0 @@
-package me.chimkenu.mangax.characters.deku;
-
-import me.chimkenu.mangax.characters.Character;
-
-public class Deku extends Character {
-}
