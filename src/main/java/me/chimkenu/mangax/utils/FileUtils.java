7b134a00175b5f43de0ee3e4573a3b259a454b50
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-07-04 14:57:26 +0800

    FileUtils woopee

---
+++ src/main/java/me/chimkenu/mangax/utils/FileUtils.java
@@ -0,0 +1,37 @@
+package me.chimkenu.mangax.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class FileUtils {
+    public static void copyDirectory(String sourceDirectory, String destinationDirectory) throws IOException {
+        try (Stream<Path> walk = Files.walk(Paths.get(sourceDirectory))) {
+            walk.forEach(source -> {
+                Path destination = Paths.get(destinationDirectory, source.toString()
+                        .substring(sourceDirectory.length()));
+
+                if (destination.toString().contains("session.lock")) {
+                    return;
+                }
+
+                try {
+                    Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+                } catch (DirectoryNotEmptyException ignored) {
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+
+    public static void deleteDirectory(Path root) throws IOException {
+        try (Stream<Path> walk = Files.walk(root)) {
+            walk.sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        }
+    }
+}
