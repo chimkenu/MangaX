f79f99e5544763e7f771b757f15e9cab824d92ac
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-07-07 11:05:29 +0800

    docs

--- src/main/java/me/chimkenu/mangax/characters/Activate.java
+++ src/main/java/me/chimkenu/mangax/characters/Activate.java
@@ -4,5 +4,11 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public interface Activate {
+    /**
+     * Runs when a move is triggered or used for the first time or used its follow-up
+     *
+     * @param plugin a MangaX plugin instance
+     * @param entity the entity that triggered the move
+     */
     void activate(JavaPlugin plugin, LivingEntity entity);
 }
