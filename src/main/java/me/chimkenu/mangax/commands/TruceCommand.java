4e0b69bf12ee6e51eea25360b4a750417bfd11d3
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-07-07 11:31:43 +0800

    formatted texts

--- src/main/java/me/chimkenu/mangax/commands/TruceCommand.java
+++ src/main/java/me/chimkenu/mangax/commands/TruceCommand.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static net.kyori.adventure.text.Component.text;
+
 public class TruceCommand implements CommandExecutor, TabCompleter {
     private final long REQUEST_DURATION = 1000 * 30;
     private final TruceListener truceListener;
@@ -42,36 +44,38 @@ public class TruceCommand implements CommandExecutor, TabCompleter {
                     two = (LivingEntity) Bukkit.getEntity(UUID.fromString(args[1]));
                     if (one == null || two == null || one == two) throw new IllegalArgumentException();
                 } catch (IllegalArgumentException | ClassCastException ignored) {
-                    sender.sendMessage(Component.text("Something went wrong with adding these entities. Make sure the arguments are UUIDs of two valid and distinct LivingEntities.", NamedTextColor.RED));
+                    sender.sendMessage(text().content("Something went wrong with adding these entities. Make sure the arguments are UUIDs of two valid and distinct LivingEntities.").color(NamedTextColor.RED).build());
                     return true;
                 }
 
                 if (truceListener.containsTruce(one, two)) {
                     truceListener.removeTruce(one, two);
-                    sender.sendMessage(Component.text("Removed the truce from these two.", NamedTextColor.GREEN));
+                    sender.sendMessage(text().content("Removed the truce from these two.").color(NamedTextColor.GREEN).build());
                 } else {
                     truceListener.addTruce(one, two);
-                    sender.sendMessage(Component.text("Added these two to a truce.", NamedTextColor.GREEN));
+                    sender.sendMessage(text().content("Added these two to a truce.").color(NamedTextColor.GREEN).build());
                 }
                 return true;
             }
 
-            sender.sendMessage(Component.text("Only players may use this command.", NamedTextColor.RED));
+            sender.sendMessage(text().content("Only players may use this command.").color(NamedTextColor.RED).build());
             return true;
         }
 
         // player truces
         if (args.length == 0) {
-            sender.sendMessage(Component.text("Missing arguments.", NamedTextColor.RED)
-                    .append(Component.text(" Usage: /truce [add|remove|list] [player]", NamedTextColor.GRAY)));
+            sender.sendMessage(text().content("Missing arguments.").color(NamedTextColor.RED)
+                    .append(text(" Usage: /truce [add|remove|list] [player]", NamedTextColor.GRAY))
+                    .build());
             return true;
         }
 
         switch (args[0].toLowerCase()) {
             case "add" -> {
                 if (args.length < 2) {
-                    sender.sendMessage(Component.text("Please specify a player to form a truce.", NamedTextColor.RED)
-                            .append(Component.text(player.isOp() ? " (or a valid UUID!)" : "", NamedTextColor.DARK_GRAY)));
+                    sender.sendMessage(text().content("Please specify a player to form a truce.").color(NamedTextColor.RED)
+                            .append(text(player.isOp() ? " (or a valid UUID!)" : "", NamedTextColor.DARK_GRAY))
+                            .build());
                     return true;
                 }
 
@@ -83,20 +87,21 @@ public class TruceCommand implements CommandExecutor, TabCompleter {
                 }
 
                 if (target == null) {
-                    sender.sendMessage(Component.text(args[1], NamedTextColor.YELLOW)
-                            .append(Component.text(" could not be found. Did you type it correctly?", NamedTextColor.RED)));
+                    sender.sendMessage(text().content(args[1]).color(NamedTextColor.YELLOW)
+                            .append(text(" could not be found. Did you type it correctly?", NamedTextColor.RED))
+                            .build());
                     return true;
                 } else if (target == player) {
-                    sender.sendMessage(Component.text("You can't form a truce with yourself, silly!", NamedTextColor.YELLOW));
+                    sender.sendMessage(text().content("You can't form a truce with yourself, silly!").color(NamedTextColor.YELLOW).build());
                     return true;
                 } else if (!(target instanceof Player)) {
                     truceListener.addTruce(player, target);
-                    sender.sendMessage(Component.text("Formed a truce.", NamedTextColor.GREEN));
+                    sender.sendMessage(text().content("Formed a truce.").color(NamedTextColor.GREEN).build());
                     return true;
                 }
 
                 if (truceListener.containsTruce(player, target)) {
-                    sender.sendMessage(Component.text("You already have a truce with them.", NamedTextColor.YELLOW));
+                    sender.sendMessage(text().content("You already have a truce with them.").color(NamedTextColor.YELLOW).build());
                     return true;
                 }
 
@@ -104,12 +109,13 @@ public class TruceCommand implements CommandExecutor, TabCompleter {
                 for (Request request : requests) {
                     if (request.truce().equals(truce)) {
                         if (request.truce().one() == player) {
-                            sender.sendMessage(Component.text("Please wait for your request to be accepted.", NamedTextColor.RED));
+                            sender.sendMessage(text().content("Please wait for your request to be accepted.").color(NamedTextColor.RED).build());
                         } else {
                             truceListener.addTruce(player, target);
-                            sender.sendMessage(Component.text("Accepted the truce request. You have now formed a truce with ", NamedTextColor.YELLOW)
-                                    .append(target.name().color(NamedTextColor.GOLD)));
-                            target.sendMessage(player.displayName()
+                            sender.sendMessage(text().content("Accepted the truce request. You have now formed a truce with ").color(NamedTextColor.YELLOW)
+                                    .append(target.name().color(NamedTextColor.GOLD))
+                                    .build());
+                            target.sendMessage(player.displayName().color(NamedTextColor.GOLD)
                                     .append(Component.text(" has accepted your truce request. You have now formed a truce with ", NamedTextColor.YELLOW))
                                     .append(player.displayName().color(NamedTextColor.GOLD)));
                         }
@@ -119,19 +125,21 @@ public class TruceCommand implements CommandExecutor, TabCompleter {
                 }
 
                 requests.add(new Request(truce, System.currentTimeMillis()));
-                sender.sendMessage(Component.text("You have requested to form a truce with ")
-                        .append(target.name())
-                        .append(Component.text(". They have 30 seconds to respond.")));
-                target.sendMessage(player.displayName()
-                        .append(Component.text(" has requested to form a truce with you. You have 30 seconds to accept by doing ", NamedTextColor.YELLOW))
-                        .append(Component.text("/truce add ", NamedTextColor.GOLD))
+                sender.sendMessage(text().content("You have requested to form a truce with ").color(NamedTextColor.YELLOW)
+                        .append(target.name().color(NamedTextColor.GOLD))
+                        .append(text(". They have 30 seconds to respond.", NamedTextColor.YELLOW))
+                        .build());
+                target.sendMessage(player.displayName().color(NamedTextColor.GOLD)
+                        .append(text(" has requested to form a truce with you. You have 30 seconds to accept by typing ", NamedTextColor.YELLOW))
+                        .append(text("/truce add ", NamedTextColor.GOLD))
                         .append(player.name().color(NamedTextColor.GOLD)));
             }
 
             case "remove" -> {
                 if (args.length < 2) {
-                    sender.sendMessage(Component.text("Please specify a player to remove.", NamedTextColor.RED)
-                            .append(Component.text(player.isOp() ? " (or a valid UUID!)" : "", NamedTextColor.DARK_GRAY)));
+                    sender.sendMessage(text().content("Please specify a player to remove.").color(NamedTextColor.RED)
+                            .append(Component.text(player.isOp() ? " (or a valid UUID!)" : "", NamedTextColor.DARK_GRAY))
+                            .build());
                     return true;
                 }
 
@@ -143,39 +151,46 @@ public class TruceCommand implements CommandExecutor, TabCompleter {
                 }
 
                 if (target == null) {
-                    sender.sendMessage(Component.text(args[1], NamedTextColor.YELLOW)
-                            .append(Component.text(" could not be found. Did you type it correctly?", NamedTextColor.RED)));
+                    sender.sendMessage(text().content(args[1]).color(NamedTextColor.GOLD)
+                            .append(text(" could not be found. Did you type it correctly?", NamedTextColor.RED))
+                            .build());
                     return true;
                 } else if (target == player) {
-                    sender.sendMessage(Component.text("You can't remove yourself, silly!", NamedTextColor.YELLOW));
+                    sender.sendMessage(text().content("You can't remove yourself, silly!").color(NamedTextColor.YELLOW));
                     return true;
                 } else if (!(target instanceof Player)) {
                     truceListener.removeTruce(player, target);
-                    sender.sendMessage(Component.text("Removed the truce (if there was one).", NamedTextColor.GREEN));
+                    sender.sendMessage(text().content("Removed the truce (if there was one).").color(NamedTextColor.GREEN).build());
                     return true;
                 }
 
                 if (truceListener.containsTruce(player, target)) {
                     truceListener.removeTruce(player, target);
-                    sender.sendMessage(Component.text("You no longer have a truce with ", NamedTextColor.YELLOW)
-                            .append(target.name().color(NamedTextColor.GOLD)));
-                    target.sendMessage(Component.text("You no longer have a truce with ", NamedTextColor.YELLOW)
-                            .append(player.displayName().color(NamedTextColor.GOLD)));
+                    sender.sendMessage(text().content("You no longer have a truce with ").color(NamedTextColor.YELLOW)
+                            .append(target.name().color(NamedTextColor.GOLD))
+                            .build());
+                    target.sendMessage(text().content("You no longer have a truce with ").color(NamedTextColor.YELLOW)
+                            .append(player.displayName().color(NamedTextColor.GOLD))
+                            .build());
                 } else {
-                    sender.sendMessage(Component.text("You don't have an existing truce with ", NamedTextColor.RED)
-                            .append(target.name().color(NamedTextColor.GOLD)));
+                    sender.sendMessage(text().content("You don't have an existing truce with ").color(NamedTextColor.RED)
+                            .append(target.name().color(NamedTextColor.GOLD))
+                            .build());
                 }
 
             }
 
             case "list" -> {
                 player.sendMessage(Component.text("You have a truce with the following:", NamedTextColor.YELLOW));
-                truceListener.getTruceList(player).forEach(truce -> player.sendMessage(Component.text("  ")
-                        .append(truce.one() == player ? truce.two().name() : truce.one().name())));
+                truceListener.getTruceList(player).forEach(truce -> player.sendMessage(
+                        text().content("\n  ")
+                                .append(truce.one() == player ? truce.two().name().color(NamedTextColor.GOLD) : truce.one().name().color(NamedTextColor.GOLD))
+                                .build()));
             }
 
-            default -> sender.sendMessage(Component.text("Unknown arguments.", NamedTextColor.RED)
-                    .append(Component.text(" Usage: /truce [add|remove|list] [player]", NamedTextColor.GRAY)));
+            default -> sender.sendMessage(text().content("Unknown arguments.").color(NamedTextColor.RED)
+                    .append(text(" Usage: /truce [add|remove|list] [player]", NamedTextColor.GRAY))
+                    .build());
         }
 
         return true;
@@ -183,7 +198,7 @@ public class TruceCommand implements CommandExecutor, TabCompleter {
 
     private void clearExpiredRequests() {
         requests.removeIf(request -> System.currentTimeMillis() - request.timeRequested > REQUEST_DURATION);
-    };
+    }
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
