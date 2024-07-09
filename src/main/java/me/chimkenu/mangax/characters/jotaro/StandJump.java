cf1ae110dd63834c7d8a3b4c8976da500cdaeb57
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-07-09 20:03:23 +0800

    hopefully works now D:

--- src/main/java/me/chimkenu/mangax/characters/jotaro/StandJump.java
+++ src/main/java/me/chimkenu/mangax/characters/jotaro/StandJump.java
@@ -6,12 +6,30 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 public class StandJump extends Move {
     public StandJump() {
-        super((plugin, entity) -> entity.setVelocity((new Vector(entity.getVelocity().getX(), 2, entity.getVelocity().getZ())).add(entity.getLocation().getDirection().multiply(0.2))), null, 0, 12 * 20, Material.MAGENTA_GLAZED_TERRACOTTA, Component.text("Stand Jump").color(NamedTextColor.DARK_PURPLE).decorate(TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false));
+        super(null, null, 0, 12 * 20, Material.MAGENTA_GLAZED_TERRACOTTA, Component.text("Stand Jump").color(NamedTextColor.DARK_PURPLE).decorate(TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false));
+
+        this.activate = (plugin, entity) -> {
+            jump(entity);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    jump(entity);
+                }
+            }.runTaskLater(plugin, 2);
+        };
+    }
+
+    private void jump(LivingEntity entity) {
+        Vector v = entity.getVelocity();
+        v.setY(1.5);
+        entity.setVelocity(v);
     }
 
     @Override
