59c74013e594099ef085e86a2ca5ffc396034eec
chimkenu <106415170+chimkenu@users.noreply.github.com>
2024-07-12 18:48:07 +0800

    made detroit smash a punch move

--- src/main/java/me/chimkenu/mangax/characters/deku/DetroitSmash.java
+++ src/main/java/me/chimkenu/mangax/characters/deku/DetroitSmash.java
@@ -1,6 +1,7 @@
 package me.chimkenu.mangax.characters.deku;
 
 import me.chimkenu.mangax.characters.Move;
+import me.chimkenu.mangax.characters.Punch;
 import me.chimkenu.mangax.enums.MoveInfo;
 import me.chimkenu.mangax.enums.Moves;
 import me.chimkenu.mangax.events.MoveTargetEvent;
@@ -14,29 +15,16 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
-public class DetroitSmash extends Move {
+public class DetroitSmash extends Move implements Punch {
     public DetroitSmash() {
         super((plugin, entity) -> {
-            entity.damage(2, entity);
-
+            entity.damage(1, entity);
             Location loc = entity.getEyeLocation();
             ParticleEffects.create(plugin, loc.getWorld(), loc.toVector(), loc.getDirection(), 5, 10, (world, location, index) -> world.spawnParticle(Particle.SPIT, location, 1, 0, 0, 0, 0), 0);
-
-            loc.add(loc.getDirection().multiply(2));
-            for (LivingEntity e : loc.getNearbyLivingEntities(1)) {
-                if (e != entity && !e.getType().equals(EntityType.ARMOR_STAND)) {
-                    MoveTargetEvent event = new MoveTargetEvent(Moves.DEKU_DELAWARE_SMASH, entity, e, 12, new Vector());
-                    Bukkit.getPluginManager().callEvent(event);
-                    if (event.isCancelled()) {
-                        continue;
-                    }
-                    e.setVelocity(e.getVelocity().add(event.getKnockback()));
-                    e.damage(event.getDamage(), entity);
-                }
-            }
         }, null, 0, 15 * 20, Material.RAW_IRON, Component.text("Detroit Smash").color(TextColor.fromHexString("#106761")).decorate(TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false));
     }
 
@@ -48,5 +36,16 @@ public class DetroitSmash extends Move {
     @Override
     public @NotNull MoveInfo getMoveInfo() {
         return new MoveInfo(MoveInfo.Damage.HIGH, MoveInfo.Range.CLOSE, MoveInfo.Knockback.NORMAL, MoveInfo.Manoeuvre.NONE, MoveInfo.Type.SINGLE, MoveInfo.Difficulty.TYPICAL, 2, 1, 1, false);
+    }
+
+    @Override
+    public void punch(JavaPlugin plugin, LivingEntity source, LivingEntity target, boolean isFollowUp) {
+        MoveTargetEvent event = new MoveTargetEvent(Moves.DEKU_DELAWARE_SMASH, source, target, 12, new Vector());
+        Bukkit.getPluginManager().callEvent(event);
+        if (event.isCancelled()) {
+            return;
+        }
+        target.setVelocity(target.getVelocity().add(event.getKnockback()));
+        target.damage(event.getDamage(), source);
     }
 }
